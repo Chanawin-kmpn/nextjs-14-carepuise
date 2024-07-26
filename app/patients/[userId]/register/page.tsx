ab26297e33901ce8249a89040d12400846eb9a31
Upond <pond_chanawin@hotmail.com>
2024-07-26 11:50:42 +0700

    first commit

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -1,9 +1,12 @@
-import { PatientForm } from '@/components/forms/PatientForm';
-import { Button } from '@/components/ui/button';
+import RegisterForm from '@/components/forms/registerForm';
+import { getUser } from '@/lib/actions/patient.actions';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useParams } from 'next/navigation';
+import React from 'react';
 
-export default function Home() {
+const Register = async ({ params: { userId } }: SearchParamProps) => {
+	const user = await getUser(userId);
 	return (
 		<div className="flex h-screen max-h-screen">
 			{/* TODO:  OTP Verification | PasskeyModal */}
@@ -16,7 +19,7 @@ export default function Home() {
 						alt="patient"
 						className="mb-12 h-10 w-fit"
 					/>
-					<PatientForm />
+					<RegisterForm user={user} />
 
 					<div className="text-14-regular mt-20 flex justify-between">
 						<p className="justify-items-end text-dark-600 xl:text-left">
@@ -30,12 +33,14 @@ export default function Home() {
 			</section>
 
 			<Image
-				src="/assets/images/onboarding-img.png"
+				src="/assets/images/register-img.png"
 				width={1000}
 				height={1000}
 				alt="patient"
-				className="side-img max-w-[50%]"
+				className="side-img max-w-[390px]"
 			/>
 		</div>
 	);
-}
+};
+
+export default Register;
