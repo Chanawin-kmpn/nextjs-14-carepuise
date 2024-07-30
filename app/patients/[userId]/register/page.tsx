6c0531eb17509dc9d2be833f20cc34a5340f7534
Upond <pond_chanawin@hotmail.com>
2024-07-30 08:53:18 +0700

    add appointment form

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -1,15 +1,12 @@
 import RegisterForm from '@/components/forms/RegisterForm';
 import { getUser } from '@/lib/actions/patient.actions';
 import Image from 'next/image';
-import Link from 'next/link';
-import { useParams } from 'next/navigation';
 import React from 'react';
 
 const Register = async ({ params: { userId } }: SearchParamProps) => {
 	const user = await getUser(userId);
 	return (
 		<div className="flex h-screen max-h-screen">
-			{/* TODO:  OTP Verification | PasskeyModal */}
 			<section className="remove-scrollbar container ">
 				<div className="sub-container max-w-[860px] flex-1 flex-col py-10">
 					<Image
